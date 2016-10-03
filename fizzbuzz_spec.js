78ac0b12ae22163bdc63e55416832be1c8283a8e
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2016-10-03 14:13:11 +0200

    update

--- fizzbuzz_spec.js
+++ fizzbuzz_spec.js
@@ -3,9 +3,17 @@ var expect = chai.expect;
 describe('Fizzbuzz', function () {
     it('doit initialiser un tableau', function () {
         var fizzbuzz = new FizzBuzz();
+        
         fizzbuzz.calculer();
 
         expect(fizzbuzz.resultat.length).to.equal(101);
+    });
+    
+    it('doit remplacer les multiples de 3 par fizz', function () {
+        var fizzbuzz = new FizzBuzz();
+        
+        fizzbuzz.calculer();
+
         expect(fizzbuzz.resultat[3]).to.equal('fizz');
         expect(fizzbuzz.resultat[6]).to.equal('fizz');
     });
