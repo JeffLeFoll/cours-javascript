762454c31665e7ef53a615f546b1f263c15c3f79
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2016-10-04 09:27:49 +0200

    fin de l'exercie

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,8 +9,15 @@
         calculer: function () {
             for (var i = 0; i <= 100; i++) {
 
-                if (i % 3 === 0) {
+                if (i % 3 === 0 && i % 5 === 0) {
+                    this.resultat.push('fizzbuzz');
+                    
+                } else if (i % 3 === 0) {
                     this.resultat.push('fizz');
+                    
+                } else if (i % 5 === 0) {
+                    this.resultat.push('buzz');
+                    
                 } else {
                     this.resultat.push(i);
                 }
