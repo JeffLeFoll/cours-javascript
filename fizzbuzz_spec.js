9e32ebb11cc6b6389754d6ed3cf8402103bd8c6f
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2016-10-04 09:26:48 +0200

    fin de l'exercice

--- fizzbuzz_spec.js
+++ fizzbuzz_spec.js
@@ -17,4 +17,20 @@ describe('Fizzbuzz', function () {
         expect(fizzbuzz.resultat[3]).to.equal('fizz');
         expect(fizzbuzz.resultat[6]).to.equal('fizz');
     });
+    
+    it('doit remplacer les multiples de 5 par buzz', function () {
+        var fizzbuzz = new FizzBuzz();
+        
+        fizzbuzz.calculer();
+
+        expect(fizzbuzz.resultat[5]).to.equal('buzz');
+    });
+    
+    it('doit remplacer les multiples de 3 et 5 par fizzbuzz', function () {
+        var fizzbuzz = new FizzBuzz();
+        
+        fizzbuzz.calculer();
+
+        expect(fizzbuzz.resultat[15]).to.equal('fizzbuzz');
+    });
 });
